b31aa37a69f3c5d6895242a222c6f38d1316a520
nale <93458378+nextrektor@users.noreply.github.com>
2023-03-14 18:30:01 +0700

    FInish All Without Clean COde

--- 0706012110010-Alfonsus Reklin Natanael-AFL 1/main.swift
+++ 0706012110010-Alfonsus Reklin Natanael-AFL 1/main.swift
@@ -2,10 +2,13 @@
 //  main.swift
 //  0706012110010-Alfonsus Reklin Natanael-AFL 1
 //
-//  Created by Nael :( on 28/02/23.
+//  Created by Nael on 28/02/23.
 //
 
 import Foundation
+
+
+// [][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][] //
 
 //import CoreGraphics // To detect key event
 //import Cocoa
@@ -28,22 +31,19 @@ import Foundation
 //    }
 //}
 
+// [][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][] //
 
-// Template Monster
-struct monster {
-    var name: String
-    var level: Int
-    var health: Int
-}
+// CODE ABOVE IS USED TO DETECT KEYBOARD VALUE, ESPECIALLY TO DETECT RETURN //
+// I FOUND EASIER WAY, BUT I DON'T WANT TO DELETE IT JUST YET //
 
-// Player
+
+//          PLAYER           //
+
 var playerHealth = 100
 var playerMana = 100
-
-// Monster
-var rama = monster(name: "Rama", level: 2, health: 100)
-var marcell = monster(name: "Marcell", level: 10, health: 100)
-var evan = monster(name: "Mr. Evan", level: 999999999999, health: 999999999)
+var hpPotion = 5
+var mpPotion = 5
+var name = String("Young Hero") // => Default Name
 
 let Skills = [
     "Physical Attack. No Mana Required. Deal 1pt - 10pt of damage.",
@@ -54,82 +54,19 @@ let Skills = [
     "Reetttrrreaaatttt!!!!."
 ]
 
-// Variable Repeating Menu
-//var pilih: String
 
-// Variable Nama
-var name = String("Young Hero")
+//          MONSTER           //
 
-
-// Function To-Do
-func fight(pilih: String, monsterAcak: monster) -> Int {
-    // Chance to drop item??
-    
-    // Variable Damage
-    var damage: Int = 0
-    var damageToPlayer: Int = 0
-    
-    func EnemyDamage() {
-        damageToPlayer = Int.random(in: 0..<70)
-        playerHealth = playerHealth - damageToPlayer
-        print()
-        print("~~~ Enemy's turn ~~~")
-        print("\(monsterAcak.name) deal \(damageToPlayer) damage!!")
-        print()
-        print("Your HP: \(playerHealth)")
-        print("Your MP: \(playerMana)")
-    }
-    
-    print()
-    switch pilih {
-        
-    // Physical Attack
-    case "1":
-        damage = Int.random(in: 1..<11)
-        print("You dealt \(damage) damage!!")
-        EnemyDamage()
-        
-    // Meteor
-    case "2":
-        if playerMana < 15 {
-            print("Your MP is not enough!!!")
-        } else {
-            damage = Int.random(in: 50..<101)
-            print("You dealt \(damage) damage!!")
-            playerMana = playerMana - 15
-            EnemyDamage()
-        }
-        
-    // Shield
-    case "3":
-        if playerMana < 10 {
-            print("Your MP is not enough!!!")
-        } else {
-            playerMana = playerMana - 10
-            damage = 0
-            damageToPlayer = 0
-            print("You block enemy's damage!!")
-            print()
-        }
-        
-    // Cheat Killermove - Insta Kill (Development Only)
-    case "killermove":
-        damage = monsterAcak.health
-        print("You get a help from War God ⚡️, Marcell")
-        print("The enemy died instantly 🪦")
-    default:
-        print("Please input correctly!")
-    }
-    return damage
+// Template Monster
+struct monster {
+    var name: String
+    var level: Int
+    var health: Int
 }
 
-
-func Heal() {
-    
-}
-
-
-// Function Fix
+var rama = monster(name: "Rama", level: 2, health: 100)
+var marcell = monster(name: "Marcell", level: 10, health: 100)
+var evan = monster(name: "Mr. Evan", level: 999999999999, health: 999999999)
 
 func MonsterGenerator() -> monster {
     let random = Int.random(in: 0..<3)
@@ -144,6 +81,149 @@ func MonsterGenerator() -> monster {
     }
     return mob
 }
+
+
+//          BATTLE           //
+
+func fight(pilih: String, monsterAcak: monster) -> Int {
+    
+    // Variable Battle
+    var damage: Int = 0
+    var damageToPlayer: Int = 0
+    
+    func EnemyDamage() {
+        damageToPlayer = Int.random(in: 0..<11)
+        playerHealth -= damageToPlayer
+        print()
+        print("~~~ Enemy's turn ~~~")
+        print("\(monsterAcak.name) deal \(damageToPlayer) damage!!")
+        checkHpMp()
+    }
+    
+    print()
+    switch pilih {
+        
+    // Physical Attack
+    case "1":
+        damage = Int.random(in: 1..<11)
+        print("You dealt \(damage) damage!!")
+        dropPotion()
+        if (monsterAcak.health - damage) > 0 {
+            EnemyDamage()
+        } else {
+            print()
+            print("You've defeat \(monsterAcak.name)")
+            print()
+        }
+        
+    // Meteor
+    case "2":
+        if playerMana < 15 {
+            print("Your MP is not enough!!!")
+        } else {
+            damage = Int.random(in: 50..<101)
+            print("You dealt \(damage) damage!!")
+            dropPotion()
+            playerMana -= 15
+            if (monsterAcak.health - damage) > 0 {
+                EnemyDamage()
+            } else {
+                print()
+                print("You've defeat \(monsterAcak.name)")
+                print()
+            }
+        }
+        
+    // Shield
+    case "3":
+        if playerMana < 10 {
+            print("Your MP is not enough!!!")
+        } else {
+            playerMana -= 10
+            damage = 0
+            damageToPlayer = 0
+            print("You block enemy's damage!!")
+            print()
+        }
+        
+    // Heal HP
+    case "4":
+        Heal().healHP()
+        EnemyDamage()
+        
+    case "5":
+        Heal().regenerateMP()
+        EnemyDamage()
+        
+    case "6":
+        Flee()
+        
+    // Cheat Killermove - Insta Kill (Development Only)
+    case "killermove":
+        damage = monsterAcak.health
+        print("You get a help from War God ⚡️, Marcell")
+        print("The enemy died instantly 🪦")
+        
+    default:
+        print("Please input correctly!")
+    }
+    return damage
+}
+
+
+//          HEAL & POTION           //
+
+func dropPotion() {
+    let dropChance = Int.random(in: 0..<201)
+    let totalPotion = Int.random(in: 1..<6)
+    print("[\(dropChance)]")
+    if (25...76).contains(dropChance) {
+        hpPotion += totalPotion
+        print("You have obtained HP Potion \(totalPotion)x")
+        checkPotion()
+    } else if (125...176).contains(dropChance) {
+        mpPotion += totalPotion
+        print("You have obtained MP Potion \(totalPotion)x")
+        checkPotion()
+    }
+}
+
+func checkHpMp () {
+    print()
+    print("Your HP: \(playerHealth)")
+    print("Your MP: \(playerMana)")
+    print()
+}
+
+func checkPotion () {
+    print()
+    print("Your HP Potion: \(hpPotion)")
+    print("Your MP Potion: \(mpPotion)")
+    print()
+}
+
+
+func Heal() -> (healHP: () -> Void, regenerateMP: () -> Void) {
+    
+    // This is Tuple of Functions - Coba2 soalnya udah kebanyakan function, next time bakal pecah file aja
+    
+    func healHP() {
+        playerHealth += 20
+        print("Your HP healed by 20!")
+        checkHpMp()
+    }
+    
+    func regenerateMP() {
+        playerMana += 10
+        print("Your MP restored by 10!")
+        checkHpMp()
+    }
+    
+    return (healHP, regenerateMP)
+}
+
+
+//          SCREEN           //
 
 func Opening () {
     while(true) {
@@ -194,6 +274,10 @@ func JourneyScreen() {
         print("Your choice?")
         let pilih = readLine()?.uppercased()
         switch pilih {
+        case "C":
+            PlayerStats()
+        case "H":
+            Heal().healHP()
         case "F":
             ForestTroll()
         case "M":
@@ -215,37 +299,37 @@ func JourneyScreen() {
 }
 
 func PlayerStats() {
-    print("Player name : \(name)")
-    print()
-    print("HP: \(playerHealth) / 100")
-    print("MP: \(playerMana) / 100")
-    print()
-
-    print("Skills:")
-    for (index, skill) in Skills.enumerated() {
-        if (index < 3) {
-            print("[\(index + 1)] \(skill)")
-            if (index == 2) {
-                print()
+    while (true) {
+        print()
+        print("Player name : \(name)")
+        print()
+        print("HP: \(playerHealth) / 100")
+        print("MP: \(playerMana) / 100")
+        print()
+        
+        print("Skills:")
+        for (index, skill) in Skills.enumerated() {
+            if (index < 3) {
+                print("[\(index + 1)] \(skill)")
+                if (index == 2) {
+                    print()
+                }
             }
         }
+        
+        print()
+        print("Items:")
+        print("- Potion x[\(hpPotion)]. Heal 20pt of your HP.")
+        print("- Elixir x[\(mpPotion)]. Regenerate 10pt of your MP.")
+        
+        print()
+        print("Press [return] to go back: ")
+        let pilih = readLine()
+        if (pilih == "") {
+            print("Returning...")
+            break
+        }
     }
-    
-    print()
-    print("Items:")
-    print("- Potion x[IntPotion]. Heal 20pt of your HP.")
-    print("- Elixir x[IntElixir]. Regenerate 10pt of your MP.")
-    
-//    print("- Physical Attack. No Mana Required. Deal 5pt of damage.")
-//    print("- Meteor. Use 15pt of MP. Deal 50pt of damage.")
-//    print("- Shield. Use 10pt of MP. Block enemy's attack in 1 turn.")
-//    print()
-//    print("Items:")
-//    print("- Potion x[IntPotion]. Heal 20pt of your HP.")
-//    print("- Elixir x[IntElixir]. Regenerate 10pt of your MP.")
-    
-    print()
-    print("Press [return] to go back: ")
 }
 
 func ForestTroll() {
@@ -259,6 +343,11 @@ func ForestTroll() {
         print("😈 Level: \(monsterAcak.level)")
         print("😈 Health: \(monsterAcak.health)")
         print()
+        print("============================================")
+        print()
+        checkHpMp()
+        checkPotion()
+        print()
         print("Choose your action:")
         print()
         for (index, skill) in Skills.enumerated() {
@@ -271,7 +360,6 @@ func ForestTroll() {
         print()
         print("Your choice?")
         if let pilih = readLine() {
-//            fight(pilih: pilih, monsterAcak: monsterAcak)
             let damage = fight(pilih: pilih, monsterAcak: monsterAcak)
             monsterAcak.health = monsterAcak.health - damage
         }
@@ -304,7 +392,6 @@ func MountainGolem() {
         print()
         print("Your choice?")
         if let pilih = readLine() {
-//            fight(pilih: pilih, monsterAcak: monsterAcak)
             let damage = fight(pilih: pilih, monsterAcak: monsterAcak)
             monsterAcak.health = monsterAcak.health - damage
         }
@@ -315,11 +402,16 @@ func MountainGolem() {
 }
 
 func Flee() {
-    print("You feel that if you don't escape soon, you won't be able to continue the fight.")
-    print("You look around frantically, searching for a way out. You sprint toward the exit and scream like a little girl.")
-    print("You're safe, for now.")
-    print("Press [return] to continue:")
-    
+    while (true) {
+        print("You feel that if you don't escape soon, you won't be able to continue the fight.")
+        print("You look around frantically, searching for a way out. You sprint toward the exit and scream like a little girl.")
+        print("You're safe, for now.")
+        print("Press [return] to continue:")
+        let pilih = readLine()
+        if (pilih == "") {
+            break
+        }
+    }
 }
 
 func Exit() {
@@ -341,135 +433,10 @@ func Exit() {
 }
 
 
-// Main View
+//          MAIN VIEW           //
+
 while(true) {
     Opening()
     Welcome()
     JourneyScreen()
 }
-
-
-// Homescreen
-//while u < 1 {
-//
-//    // Monster Randomizer
-//    let random = Int.random(in: 0..<3)
-//    var mob: monster
-//    switch random {
-//    case 0:
-//        mob = rama
-//    case 1:
-//        mob = marcell
-//    default:
-//        mob = evan
-//    }
-//
-//
-//
-//    // Tampilan Awal
-//    u2 = 0
-//    let home = "Welcome to the Death!! 🪦🪦\n\nYou got hit by a flying chair 🪑, such a ridiculous achievement I must say! Anyways, I give you a very rare opportunity which I have never offer to anyone but you 🫵🏻. You will be sent to a world 🌏 full of laugh... (sort of)...\n\nEhem. Enjoy your trip, dear\n\nPress [ok] to continue:"
-//    print(home)
-//
-//
-//    let pilih = readLine()
-//
-//    switch pilih {
-//    case "1" :
-//
-//        // Game Start
-//        while ( u2 < 1 && mob.health > 0) {
-//
-//            let monsterString = "\n\nMonster kamu: \(mob.name) \nLevel: \(mob.level) \nHealth: \(mob.health)"
-//            print(monsterString)
-//
-//            let menu = "\n\nPilih Aksi: \n1. Pukul \n2. Tendang \n3. Kabur \n\nInput Aksi: "
-//            print(menu)
-//            let pilih2 = readLine()
-//
-//            switch pilih2 {
-//            case "1":
-//                mob.health = mob.health - 10
-//            case "2":
-//                mob.health = mob.health - 30
-//            case "3":
-//                u2 = 1
-//            case "killermove":
-//                mob.health = 0
-//            default:
-//                print("\nMohon tuliskan 1, 2, atau 3 \nInput \(String(describing: pilih2)) bukan merupakan input yang benar\n\n")
-//            }
-//
-//            if(mob.health <= 0) {
-//                print("\n==================================\nCongrats! Anda menang!!\n==================================\n")
-//            }
-//
-//        }
-//    case "2":
-//
-//        // Gacha
-//        while u2 < 1 {
-//            print("\n\nAnda belum memiliki hero apapun\nKembali? (y/n)")
-//            let back = readLine()
-//            print("\n")
-//
-//            switch back {
-//            case "y":
-//                u2 = 1
-//            case "n":
-//                u2 = 0
-//            default:
-//                print("Mohon input dengan benar")
-//            }
-//        }
-//    case "ok":
-//
-//        //        Plus side from this mehtod is I can handle space, but can't filter all the possibilities of wrong input!!!
-//        print("\n\nOh! One thing, I forgot your name. I believe you remember your name. What is your name?:")
-//        if let inputName = readLine() {
-//            if (inputName.contains("    ") || inputName.isEmpty || inputName.contains("\t")) {
-//                name = "Young Hero"
-//            } else {
-//                name = inputName
-//            }
-//        }
-//        print("\n\nOh? You will be called \(name) from now then!\nGood Luck \(name)!\n\n")
-//    case "S":
-//
-////        Plus side from this mehtod is I can filter all the possibilities of wrong input, but can't handle space!!!
-//        print("\n\nOh! One thing, I forgot your name. I believe you remember your name. What is your name?:")
-//        if let inputName = readLine(), let regex = try? NSRegularExpression(pattern: "^[a-zA-Z]+$"), regex.numberOfMatches(in: inputName, range: NSRange(inputName.startIndex..., in: inputName)) == 1 {
-//            name = inputName
-//        } else {
-//            name = "Young Hero"
-//        }
-//        print("\n\nOh? You will be called \(name) from now then!\nGood Luck \(name)!\n\n")
-//    case "J":
-//        JourneyScreen()
-//    case "":
-//            //        This doesn't work, it also detect emoji as nil
-//            print("This should be [return] statement")
-//    case "3":
-//
-//        // Exit
-//        let animationFrames = ["🐼", "🦀", "🦆", "🦜", "🦋", "🐞", "🐣", "🦥"]
-//            var frameIndex = 0
-//
-//            print("Exiting...")
-//
-//            // Print animation frames with delay
-//            for _ in 0..<10 {
-//                let frame = animationFrames[frameIndex]
-//                print("  \(frame)  Please wait...")
-//                frameIndex = (frameIndex + 1) % animationFrames.count
-//                usleep(100_000) // Sleep for 100 milliseconds
-//            }
-//
-//            exit(0)
-//    default:
-//
-//        // Error handling
-//        print("\nMohon tuliskan 1 atau 2 \nInput \(String(describing: pilih)) bukan merupakan input yang benar\n")
-//    }
-//}
-
